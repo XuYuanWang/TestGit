a0ca315e26d49044dcfc1199a9e8895248e0747a
xujun <xuj981243@gmail.com>
2020-04-15 11:19:03 +0800

    hello.java2

--- hello.java
+++ hello.java
@@ -1,5 +1,9 @@
 class Hello{
 	public static void main(String[] args){
 		System.out.print("Hello World");
+		System.out.print("1234");
+		System.out.print("5678");
+		System.out.print("1357");
+		System.out.print("2468");
 	}
 }
